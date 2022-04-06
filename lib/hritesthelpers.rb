7662bb9ea78d8b03550b480a2c2609f87aec3a25
Fred Ricci <fjricci@us.ibm.com>
2022-04-06 09:50:54 -0400

    Add an Event Streams helper that uses the Admin API

--- lib/hritesthelpers.rb
+++ lib/hritesthelpers.rb
@@ -1,6 +1,7 @@
 require_relative 'hritesthelpers/app_id_helper'
 require_relative 'hritesthelpers/cos_helper'
 require_relative 'hritesthelpers/elastic_helper'
+require_relative 'hritesthelpers/event_streams_api_helper'
 require_relative 'hritesthelpers/event_streams_helper'
 require_relative 'hritesthelpers/flink_helper'
 require_relative 'hritesthelpers/iam_helper'
